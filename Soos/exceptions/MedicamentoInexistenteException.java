62e9b4557a7ed510327d5e93fa7585deb764240d
mainaracdf <mainaracdf@lcc2-12.lcc.ufcg.edu.br>
2016-04-28 10:26:56 -0300

    Exceptions de Farmacia

---
+++ Soos/exceptions/MedicamentoInexistenteException.java
@@ -0,0 +1,14 @@
+package exceptions;
+
+public class MedicamentoInexistenteException extends LogicaException{
+
+	private static final long serialVersionUID = 1L;
+    
+    public MedicamentoInexistenteException() {
+        super("Esse medicamento nao existe!");
+    }
+      
+    public MedicamentoInexistenteException(String mensagem) {
+        super(mensagem);
+    }
+}
---
+++ Soos/exceptions/MedicamentoJaCadastradoException.java
@@ -0,0 +1,14 @@
+package exceptions;
+
+public class MedicamentoJaCadastradoException extends LogicaException {
+
+	 private static final long serialVersionUID = 1L;
+     
+	    public MedicamentoJaCadastradoException() {
+	        super("Esse medicamento ja foi cadastrado!");
+	    }
+	      
+	    public MedicamentoJaCadastradoException(String mensagem) {
+	        super(mensagem);
+	    }
+}
