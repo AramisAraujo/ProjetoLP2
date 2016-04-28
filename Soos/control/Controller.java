0695bb0c39f4a38cf76c8b988205940b89da69b7
AramisAraujo <aramis.araujo@ccc.ufcg.edu.br>
2016-04-28 11:53:54 -0300

    Yay

--- Soos/control/Controller.java
+++ Soos/control/Controller.java
@@ -1,5 +1,20 @@
 package control;
 
+import java.util.HashMap;
+
 public class Controller {
+	
+	private boolean bloqueioInicial;
+	private Usuario usuarioAtual;
+	private Map<int, Usuario> bancoUsuarios;
+	private cadstraFuncionario(String nome, tipoCargo cargo, int dataNascimento);
+	private gerarSenha(int anoNascimento, int matricula);
+	private getUsuario(int matricula);
+	private removerUsuario(int matricula);
+	private atualizar
+	
+	
+	
+	
 
 }
