e045dd6ee8845dc10a73e582b27306812f072141
gabrielac <gabrielac@lcc2-10.lcc.ufcg.edu.br>
2016-04-28 11:49:53 -0300

    Parte Usuario

--- Soos/usuario/FactoryUsuario.java
+++ Soos/usuario/FactoryUsuario.java
@@ -1,7 +1,7 @@
 package usuario;
 
 public class FactoryUsuario {
-	public Usuario criarUsuario(String nome, int dataNascimento, int senha, int matricula, TipoCargo cargo) {
+	public Usuario criarUsuario(String nome, int dataNascimento, int senha, int matricula, TipoCargo cargo) throws Exception {
 		switch (cargo) {
 			case DIRETOR:
 				return new Diretor(nome, dataNascimento, senha, matricula);
@@ -9,6 +9,8 @@ public class FactoryUsuario {
 				return new Medico(nome, dataNascimento, senha, matricula);
 			case TECNICOADM:
 				return new TecnicoAdm(nome, dataNascimento, senha, matricula);
-		};
+			default:
+				throw new Exception();
+		}
 	}
 }
