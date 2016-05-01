8b029d4f82ccd1c390e750cd54f16a265b080214
Aramis Araujo <aramis.araujo@ccc.ufcg.edu.br>
2016-05-01 11:19:19 -0300

    Atualização

--- Soos/control/Controller.java
+++ Soos/control/Controller.java
@@ -1,34 +1,80 @@
 package control;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import usuario.Usuario;
+import usuario.TipoCargo;
 
 public class Controller {
 	
-	private boolean bloqueioInicial;
+	private boolean sistemaBloqueado;
 	private Usuario usuarioAtual;
-	private Map<int, Usuario> bancoUsuarios;
+	private Map bancoUsuarios;
 	
 	public void iniciaSistema(){
 		
 	}
-	
-	public boolean liberaSistema(String chave, String nome, String dataNascimento){
+	public void fechaSistema(){
 		
 	}
-	private boolean cadstraFuncionario(String nome, tipoCargo cargo, String dataNascimento){
+	
+	public boolean liberaSistema(String chave, String nome, String dataNascimento){
+		if(sistemaBloqueado == false){
+			//throw exception sistema jah liberado
+		}
+		if(!chave.equals("c041ebf8")){
+			//throw exception chave errada
+		}
+		//cadastrar diretor
+		this.sistemaBloqueado = false;
+		
+		return false;
+		
+	}
+	private boolean cadstraFuncionario(String nome, String cargo, String dataNascimento){
+		return true;
+		
+	}
+	
+	public String getinfoFuncionario(int matricula, String info){
 		
 	}
 	private void gerarSenha(int anoNascimento, int matricula){
 		
 	}
 	private Usuario getUsuario(int matricula){
+		return true;
 		
 	}
 	private boolean removerUsuario(int matricula){
+		return true;
 		
 	}
 	private boolean atualizarInfo(){
+		return true;
 		
+	}
+	
+	private Date stringToDate(String dateCandidate){
+		
+		DateFormat formato = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+		Date date;
+		try {
+			date = formato.parse(dateCandidate);
+			return date;
+
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
 	}
 	
 	
