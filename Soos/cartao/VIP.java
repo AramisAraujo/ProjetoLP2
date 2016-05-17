c4b5c753483a8bace75abd824be6f6a4e474ade0
AramisAraujo <aramis.araujo@ccc.ufcg.edu.br>
2016-05-17 07:43:38 -0300

    Bonus do cartão corrigido.

--- Soos/cartao/VIP.java
+++ Soos/cartao/VIP.java
@@ -3,7 +3,7 @@ package cartao;
 public class VIP implements Fidelidade{
 	
 	private final double TAXA_DISCONTO = 0.7;
-	private final double BONUS_PONTOS = 0.9;
+	private final double BONUS_PONTOS = 1.10;
 
 	@Override
 	public int calcularPontos(int pontos) {
